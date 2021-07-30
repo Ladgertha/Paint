90619e2edd572feb375e50b3f13acead62fb8cc7
Anna <myandroidblog@yandex.ru>
2021-07-30 19:17:19 +0300

    Paint is done

--- app/src/main/java/ru/ladgertha/paint/MainActivity.kt
+++ app/src/main/java/ru/ladgertha/paint/MainActivity.kt
@@ -1,11 +1,21 @@
 package ru.ladgertha.paint
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val customView = MyCanvasView(this)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, customView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+        customView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(customView)
     }
 }
